5342bfb1e998c9120f8a72bc811f2907ede8ad87
Andrew Gunnerson <accounts+github@chiller3.com>
2023-07-29 16:46:53 -0400

    Switch to shorter, machine-readable SPDX license headers
    Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

--- app/src/main/cpp/custota_selinux/include/finally.h
+++ app/src/main/cpp/custota_selinux/include/finally.h
@@ -1,20 +1,8 @@
 /*
  * Copyright (C) 2014-2023  Andrew Gunnerson <andrewgunnerson@gmail.com>
  *
- * This file is part of Custota, based on DualBootPatcher code.
- *
- * Custota is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Custota is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Custota.  If not, see <http://www.gnu.org/licenses/>.
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ * Based on DualBootPatcher code.
  */
 
 #pragma once
