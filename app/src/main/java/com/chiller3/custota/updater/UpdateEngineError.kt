chenxiaolong/Custota

commit ebf1518eadd33e034cb54b7016c3217dfc4ee93c

Show notification for post-reboot cleanup process after OTA update
On virtual A/B devices, after an OTA is installed and the device is
rebooted, the system will merge the new data from the copy-on-write
volumes to the actual partitions. This process takes a few minutes and
most users aren't aware that it is happening.

It also causes some weird behavior with Custota. If the user tried to
check for updates during this process, the notification would say the
updater is initializing for a few minutes and then report that the OTA
was successfully installed (again). Let's fix this by showing what is
actually going on.

This commit also fixes a couple other notification issues:

* The UpdaterThread.ProgressType.INIT state has been removed since it
  races with onStatusUpdate() and sometimes causes the initialization
  message to be shown longer than necessary (until the next progress
  update). It is not necessary anyway since UpdaterService shows the
  initialization message when it hasn't received any state yet.
* Only show the pause, resume, and cancel notification actions during
  the UPDATE, VERIFY, and FINALIZE states because those are the only
  states where they would work anyway.

Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

app/src/main/java/com/chiller3/custota/updater/UpdateEngineError.kt
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2023 Andrew Gunnerson
+ * SPDX-FileCopyrightText: 2023-2024 Andrew Gunnerson
  * SPDX-License-Identifier: GPL-3.0-only
  */
 
@@ -72,6 +72,9 @@ object UpdateEngineError {
     const val DEVICE_CORRUPTED = 61
     const val PACKAGE_EXCLUDED_FROM_UPDATE = 62
     const val POST_INSTALL_MOUNT_ERROR = 63
+    const val OVERLAYFS_ENABLED_ERROR = 64
+    const val UPDATE_PROCESSING = 65
+    const val UPDATE_ALREADY_INSTALLED = 66
 
     private val STRINGS = arrayOf(
         UpdateEngineError::SUCCESS.name,
@@ -138,6 +141,9 @@ object UpdateEngineError {
         UpdateEngineError::DEVICE_CORRUPTED.name,
         UpdateEngineError::PACKAGE_EXCLUDED_FROM_UPDATE.name,
         UpdateEngineError::POST_INSTALL_MOUNT_ERROR.name,
+        UpdateEngineError::OVERLAYFS_ENABLED_ERROR.name,
+        UpdateEngineError::UPDATE_PROCESSING.name,
+        UpdateEngineError::UPDATE_ALREADY_INSTALLED,
     )
 
     /**

app/src/main/java/com/chiller3/custota/updater/UpdateEngineStatus.kt
@@ -1,11 +1,11 @@
 /*
- * SPDX-FileCopyrightText: 2023 Andrew Gunnerson
+ * SPDX-FileCopyrightText: 2023-2024 Andrew Gunnerson
  * SPDX-License-Identifier: GPL-3.0-only
  */
 
 package com.chiller3.custota.updater
 
-/** Must match AOSP's system/update_engine/common/error_code.h. */
+/** Must match AOSP's system/update_engine/client_library/include/update_engine/update_status.h. */
 @Suppress("MemberVisibilityCanBePrivate")
 object UpdateEngineStatus {
     const val IDLE = 0
@@ -18,6 +18,8 @@ object UpdateEngineStatus {
     const val REPORTING_ERROR_EVENT = 7
     const val ATTEMPTING_ROLLBACK = 8
     const val DISABLED = 9
+    const val NEED_PERMISSION_TO_UPDATE = 10
+    const val CLEANUP_PREVIOUS_UPDATE = 11
 
     private val STRINGS = arrayOf(
         UpdateEngineStatus::IDLE.name,
@@ -30,6 +32,8 @@ object UpdateEngineStatus {
         UpdateEngineStatus::REPORTING_ERROR_EVENT.name,
         UpdateEngineStatus::ATTEMPTING_ROLLBACK.name,
         UpdateEngineStatus::DISABLED.name,
+        UpdateEngineStatus::NEED_PERMISSION_TO_UPDATE.name,
+        UpdateEngineStatus::CLEANUP_PREVIOUS_UPDATE.name,
     )
 
     init {

app/src/main/AndroidManifest.xml
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!--
-    SPDX-FileCopyrightText: 2023 Andrew Gunnerson
+    SPDX-FileCopyrightText: 2023-2024 Andrew Gunnerson
     SPDX-License-Identifier: GPL-3.0-only
 -->
 <manifest xmlns:android="http://schemas.android.com/apk/res/android"
@@ -46,6 +46,15 @@
             android:name=".updater.UpdaterService"
             android:foregroundServiceType="specialUse"
             android:exported="false" />
+
+        <receiver
+            android:name=".updater.UpdaterBootReceiver"
+            android:enabled="true"
+            android:exported="true">
+            <intent-filter>
+                <action android:name="android.intent.action.BOOT_COMPLETED" />
+            </intent-filter>
+        </receiver>
 
         <activity
             android:name=".settings.SettingsActivity"

app/src/main/java/com/chiller3/custota/updater/UpdaterBootReceiver.kt
@@ -1,0 +1,21 @@
+/*
+ * SPDX-FileCopyrightText: 2024 Andrew Gunnerson
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
+
+package com.chiller3.custota.updater
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+class UpdaterBootReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context, intent: Intent?) {
+        if (intent?.action != Intent.ACTION_BOOT_COMPLETED) {
+            return
+        }
+
+        // This will monitor the cleanup process on the reboot immediately following an OTA.
+        UpdaterJob.scheduleImmediate(context, UpdaterThread.Action.CHECK)
+    }
+}

app/src/main/java/com/chiller3/custota/updater/UpdaterService.kt
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: 2023 Andrew Gunnerson
+ * SPDX-FileCopyrightText: 2023-2024 Andrew Gunnerson
  * SPDX-License-Identifier: GPL-3.0-only
  */
 
@@ -165,25 +165,28 @@ class UpdaterService : Service(), UpdaterThread.UpdaterThreadListener {
         Log.d(TAG, "Updating foreground notification for state: $state")
 
         val titleResId = when (state?.type) {
-            UpdaterThread.ProgressType.INIT, null -> R.string.notification_state_init
+            null -> R.string.notification_state_init
             UpdaterThread.ProgressType.CHECK -> R.string.notification_state_check
             UpdaterThread.ProgressType.UPDATE -> R.string.notification_state_install
             UpdaterThread.ProgressType.VERIFY -> R.string.notification_state_verify
             UpdaterThread.ProgressType.FINALIZE -> R.string.notification_state_finalize
+            UpdaterThread.ProgressType.CLEANUP -> R.string.notification_state_cleanup
         }
         val actionResIds = mutableListOf<Int>()
         val actionIntents = mutableListOf<Intent>()
 
-        updaterThread?.let { thread ->
-            if (thread.isPaused) {
-                actionResIds.add(R.string.notification_action_resume)
-                actionIntents.add(createActionIntent(ACTION_RESUME))
-            } else {
-                actionResIds.add(R.string.notification_action_pause)
-                actionIntents.add(createActionIntent(ACTION_PAUSE))
+        if (state?.type?.isActionable == true) {
+            updaterThread?.let { thread ->
+                if (thread.isPaused) {
+                    actionResIds.add(R.string.notification_action_resume)
+                    actionIntents.add(createActionIntent(ACTION_RESUME))
+                } else {
+                    actionResIds.add(R.string.notification_action_pause)
+                    actionIntents.add(createActionIntent(ACTION_PAUSE))
+                }
+                actionResIds.add(R.string.notification_action_cancel)
+                actionIntents.add(createActionIntent(ACTION_CANCEL))
             }
-            actionResIds.add(R.string.notification_action_cancel)
-            actionIntents.add(createActionIntent(ACTION_CANCEL))
         }
 
         val notification = notifications.createPersistentNotification(
@@ -215,6 +218,11 @@ class UpdaterService : Service(), UpdaterThread.UpdaterThreadListener {
         val showReboot: Boolean
 
         when (result) {
+            is UpdaterThread.UpdateCleanedUp -> {
+                // No need to bug the user about this since it's automatic and not directly caused
+                // in response to a user action.
+                return
+            }
             is UpdaterThread.UpdateAvailable -> {
                 channel = Notifications.CHANNEL_ID_CHECK
                 // Only bug the user once while the notification is still shown

app/src/main/java/com/chiller3/custota/updater/UpdaterThread.kt
@@ -101,6 +101,7 @@ class UpdaterThread(
                 UpdateEngineStatus.DOWNLOADING -> ProgressType.UPDATE
                 UpdateEngineStatus.VERIFYING -> ProgressType.VERIFY
                 UpdateEngineStatus.FINALIZING -> ProgressType.FINALIZE
+                UpdateEngineStatus.CLEANUP_PREVIOUS_UPDATE -> ProgressType.CLEANUP
                 else -> null
             }?.let {
                 listener.onUpdateProgress(this@UpdaterThread, it, current, max)
@@ -647,8 +648,6 @@ class UpdaterThread(
         try {
             wakeLock.acquire()
 
-            listener.onUpdateProgress(this, ProgressType.INIT, 0, 0)
-
             Log.d(TAG, "Waiting for initial engine status")
             val status = waitForStatus { it != -1 }
             val statusStr = UpdateEngineStatus.toString(status)
@@ -707,8 +706,13 @@ class UpdaterThread(
                 Log.d(TAG, "Update engine result: $errorStr")
 
                 if (UpdateEngineError.isUpdateSucceeded(error)) {
-                    Log.d(TAG, "Successfully completed upgrade")
-                    listener.onUpdateResult(this, UpdateSucceeded)
+                    if (status == UpdateEngineStatus.CLEANUP_PREVIOUS_UPDATE) {
+                        Log.d(TAG, "Successfully cleaned up upgrade")
+                        listener.onUpdateResult(this, UpdateCleanedUp)
+                    } else {
+                        Log.d(TAG, "Successfully completed upgrade")
+                        listener.onUpdateResult(this, UpdateSucceeded)
+                    }
                 } else if (error == UpdateEngineError.USER_CANCELED) {
                     Log.w(TAG, "User cancelled upgrade")
                     listener.onUpdateResult(this, UpdateCancelled)
@@ -804,6 +808,10 @@ class UpdaterThread(
         override val isError = false
     }
 
+    data object UpdateCleanedUp : Result {
+        override val isError = false
+    }
+
     /** Update succeeded in a previous updater run. */
     data object UpdateNeedReboot : Result {
         override val isError = false
@@ -822,11 +830,14 @@ class UpdaterThread(
     }
 
     enum class ProgressType {
-        INIT,
         CHECK,
         UPDATE,
         VERIFY,
         FINALIZE,
+        CLEANUP;
+
+        val isActionable: Boolean
+            get() = this == UPDATE || this == VERIFY || this == FINALIZE
     }
 
     interface UpdaterThreadListener {

app/src/main/res/values/strings.xml
@@ -90,6 +90,7 @@
     <string name="notification_state_install">Installing OTA update</string>
     <string name="notification_state_verify">Verifying OTA update</string>
     <string name="notification_state_finalize">Finalizing OTA update</string>
+    <string name="notification_state_cleanup">Cleaning up OTA update</string>
     <string name="notification_update_init_failed">Failed to initialize OTA updater</string>
     <string name="notification_update_ota_available">OTA update is available</string>
     <string name="notification_update_ota_unnecessary">OS is already up to date</string>

commit 1f12f52384960b8b369fe96c822ca651b7d9af81

UpdateEngine{Error,Status}: Fix incorrect assert on STRINGS length
This is a regression introduced in commit
ebf1518eadd33e034cb54b7016c3217dfc4ee93c.

Issue: #167

Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

app/src/main/java/com/chiller3/custota/updater/UpdateEngineError.kt
@@ -142,7 +142,7 @@ object UpdateEngineError {
         UpdateEngineError::POST_INSTALL_MOUNT_ERROR.name,
         UpdateEngineError::OVERLAYFS_ENABLED_ERROR.name,
         UpdateEngineError::UPDATE_PROCESSING.name,
-        UpdateEngineError::UPDATE_ALREADY_INSTALLED,
+        UpdateEngineError::UPDATE_ALREADY_INSTALLED.name,
     )
 
     /**
@@ -155,7 +155,7 @@ object UpdateEngineError {
     )
 
     init {
-        assert(STRINGS.size == POST_INSTALL_MOUNT_ERROR + 1)
+        assert(STRINGS.size == UPDATE_ALREADY_INSTALLED + 1)
     }
 
     fun toString(code: Int): String {

app/src/main/java/com/chiller3/custota/updater/UpdateEngineStatus.kt
@@ -36,7 +36,7 @@ object UpdateEngineStatus {
     )
 
     init {
-        assert(STRINGS.size == DISABLED + 1)
+        assert(STRINGS.size == CLEANUP_PREVIOUS_UPDATE + 1)
     }
 
     @JvmStatic