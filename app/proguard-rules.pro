chenxiaolong/Custota

commit ca94b0d31a5348346dfa3da639d62678920ff7d0

Enable proguard obfuscation
Previously, this was disabled because the app is open source and there
is no need to mask information in stack traces. However, optimizations
in recent r8 versions make the stack traces quite useless anyway, even
without obfuscation. Given that, let's switch back to the default
behavior so that at least we can take advantage of smaller APK sizes.

The mapping files will be published alongside official releases so that
anyone can decode stacktraces from logs without needing a debug build.

Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

app/proguard-rules.pro
@@ -12,18 +12,6 @@
 #   public *;
 #}
 
-# Uncomment this to preserve the line number information for
-# debugging stack traces.
--keepattributes SourceFile,LineNumberTable
-
-# If you keep the line number information, uncomment this to
-# hide the original source file name.
-#-renamesourcefileattribute SourceFile
-
-# Disable obfuscation completely. As an open source project, shrinking is the
-# only goal of minification.
--dontobfuscate
-
 # We construct TreeDocumentFile via reflection in DocumentFileExtensions
 # to speed up SAF performance when doing path lookups.
 -keepclassmembers class androidx.documentfile.provider.TreeDocumentFile {

commit 71eec5419d0edf65bc2c86eaf0f9bd431f4afc5b

proguard: Keep mContext from {Single,Tree}DocumentFile
ca94b0d31a5348346dfa3da639d62678920ff7d0 introduced the regression,
which broke the looking up of local files when installing from a local
directory.

Fixes: #189

Signed-off-by: Andrew Gunnerson <accounts+github@chiller3.com>

app/proguard-rules.pro
@@ -39,6 +39,17 @@
     *;
 }
 
+# We construct TreeDocumentFile via reflection in DocumentFileExtensions
+# to speed up SAF performance when doing path lookups.
+-keepclassmembers class androidx.documentfile.provider.SingleDocumentFile {
+    private android.content.Context mContext;
+}
+-keepclassmembers class androidx.documentfile.provider.TreeDocumentFile {
+    <init>(androidx.documentfile.provider.DocumentFile, android.content.Context, android.net.Uri);
+
+    private android.content.Context mContext;
+}
+
 # Keep standalone CLI utilities
 -keep class com.chiller3.custota.standalone.* {
     void main(java.lang.String[]);